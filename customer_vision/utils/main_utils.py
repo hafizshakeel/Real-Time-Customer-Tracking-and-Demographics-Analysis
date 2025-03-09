c1db4a95522c00878cf1262261e0495903751dca
Hafiz Shakeel <hafizshakeel3495@gmail.com>
2025-03-10 01:17:38 +0900

    Add utility functions for training pipeline

---
+++ customer_vision/utils/main_utils.py
@@ -0,0 +1,56 @@
+import os.path
+import sys
+import yaml
+import base64
+
+
+from customer_vision.exception import AppException
+from customer_vision.logger import logging
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            logging.info("Read yaml file successfully")
+            return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise AppException(e, sys) from e
+
+
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+            logging.info("Successfully write_yaml_file")
+
+    except Exception as e:
+        raise AppException(e, sys)
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open("./data/" + fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(image_bytes):
+    try:
+        return base64.b64encode(image_bytes).decode('utf-8')
+    except Exception as e:
+        raise ValueError(f"Error encoding image to base64: {str(e)}")
+
+# def encodeImageIntoBase64(croppedImagePath):
+#     with open(croppedImagePath, "rb") as f:
+#         return base64.b64encode(f.read())
+
+ 
+
