ed3ad732cd7c256d31b9747fc4b7bb85cb763bfe
Hafiz Shakeel <hafizshakeel3495@gmail.com>
2025-03-10 01:10:19 +0900

    Add package dependencies and setup configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='customer_vision',
+    version='0.1.0',
+    author='Hafiz Shakeel Ahmad Awan',
+    author_email='hafizshakeel1997@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+
+)
